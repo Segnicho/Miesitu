347bd333520eada479559a116c1a4c6cce400bfa
obsi-me <felmetamuktar23@gmail.com>
2022-01-17 16:07:55 +0300

    adding homecontroller test

--- src/main/java/com/miesitu/web_project/HomeController.java
+++ src/main/java/com/miesitu/web_project/HomeController.java
@@ -5,9 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
+    @GetMapping("/home")
     public String home(){
         return "home";
+    }
+    @GetMapping("/about")
+    public String about(){
+        return "about";
     }
     
 }
