464ce36a26ebe1d5bfc387e9dd76707c68aec0f3
Habib Gemmechu <habib.gemmech@gmail.com>
2022-01-17 10:50:10 +0300

    initialing the project

---
+++ src/main/java/com/miesitu/web_project/WebProjectApplication.java
@@ -0,0 +1,13 @@
+package com.miesitu.web_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebProjectApplication.class, args);
+	}
+
+}
