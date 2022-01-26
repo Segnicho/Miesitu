6a4d2ab775f1e405a2086cc1d020cb84fa0f34aa
obsi-me <felmetamuktar23@gmail.com>
2022-01-26 19:17:16 +0300

    User Test

--- src/main/java/com/miesitu/web_project/entity/User.java
+++ src/main/java/com/miesitu/web_project/entity/User.java
@@ -22,7 +22,7 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-
+@Builder
 public class User implements UserDetails {//implements UserDetails{
     @Id
     @GeneratedValue(
@@ -99,7 +99,4 @@ public class User implements UserDetails {//implements UserDetails{
     return true;
     }
 
-    // public User(int userId2, String username2, String firstName2, String lastName2, String email2, int phone2,
-    //         String password2, String area2, int i, String string, int j) {
-    // }
 }
